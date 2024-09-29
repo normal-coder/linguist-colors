f8adf658a5700935f72e700d86d1173e30e8ce44
诺墨 <normal@normalcoder.com>
2024-09-30 02:46:09 +0800

    test: fix Jest run failure, add test case execution time
    Signed-off-by: 诺墨 <normal@normalcoder.com>

--- test/index.test.js
+++ test/index.test.js
@@ -1,21 +1,43 @@
 import getProgrammingLangColor from "../dist/index.js";
 
-it("Will return the programming language's color (according to GitHub) in the chosen CSS format.", () => {
-    // HEX
+// HEX
+it("should return the HEX color for '1C Enterprise' language", () => {
+    console.time("Execution Time");
     expect(getProgrammingLangColor("1C Enterprise", "HEX")).toBe("#814CCC");
+    console.timeEnd("Execution Time");
+});
 
-    // RGB
+// RGB
+it("should return the RGB color for '1C Enterprise' language", () => {
+    console.time("Execution Time");
     expect(getProgrammingLangColor("1C Enterprise", "RGB")).toBe("rgb(129, 76, 204)");
+    console.timeEnd("Execution Time");
+});
 
-    // HSL
+// HSL
+it("should return the HSL color for '1C Enterprise' language", () => {
+    console.time("Execution Time");
     expect(getProgrammingLangColor("1C Enterprise", "HSL")).toBe("hsl(265, 56%, 55%)");
+    console.timeEnd("Execution Time");
+});
 
-    // HEX
+// HEX
+it("should return the HEX color for 'Odin' language", () => {
+    console.time("Execution Time");
     expect(getProgrammingLangColor("Odin", "HEX")).toBe("#60AFFE");
+    console.timeEnd("Execution Time");
+});
 
-    // RGB
+// RGB
+it("should return the RGB color for 'Odin' language", () => {
+    console.time("Execution Time");
     expect(getProgrammingLangColor("Odin", "RGB")).toBe("rgb(96, 175, 254)");
+    console.timeEnd("Execution Time");
+});
 
-    // HSL
+// HSL
+it("should return the HSL color for 'Odin' language", () => {
+    console.time("Execution Time");
     expect(getProgrammingLangColor("Odin", "HSL")).toBe("hsl(210, 99%, 69%)");
+    console.timeEnd("Execution Time");
 });
